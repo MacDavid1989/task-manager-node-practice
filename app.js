c40abe3d5819bbd7ffb83efb8a8df27029264135
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-15 17:55:49 -0400

    serve static files and remove unused code

--- app.js
+++ app.js
@@ -5,21 +5,11 @@ const connectDB = require("./db/connect");
 require("dotenv").config();
 
 // middleware
-
+app.use(express.static("./public"));
 app.use(express.json());
 
 // routes
-app.get("/hello", (req, res) => {
-  res.send("Task Manager App");
-});
-
 app.use("/api/v1/tasks", tasks);
-
-// app.get('/api/v1/tasks')        - get all the tasks
-// app.post('/api/v1/tasks')       - create new task
-// app.get('/api/v1/tasks/:id')    - get single task
-// app.patch('/api/v1/tasks/:id')  - update task
-// app.delete('/api/v1/tasks/:id') - delete task
 
 const port = 3000;
 
